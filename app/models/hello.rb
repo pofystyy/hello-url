f6ff6e0d5d85f0aa5693208cdc5dd2f987f9ba17
pofystyy <pofystyy@gmail.com>
2018-09-08 18:12:08 +0300

    add tags

--- app/models/hello.rb
+++ app/models/hello.rb
@@ -3,5 +3,17 @@ class Hello
   field :url, type: String
   field :summary, type: String
 
+  has_and_belongs_to_many :tags 
+
   validates_presence_of :url, :summary
+
+  def all_tags
+		self.tags.map(&:name).join(', ')	
+	end
+
+	def all_tags=(names)
+		self.tags = names.split(',').map do |name|
+			Tag.where(name: name.strip).first_or_create!
+		end
+	end
 end
